da44905342dd69053bec855dc2b042198ec1f003
Mehmet Akif TEZCAN <akif.tezcan@aurorabilisim.com>
2022-01-18 10:57:45 +0300

    first commit

---
+++ Aur/MyCronModule/Cron/MyCron.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Aur\MyCronModule\Cron;
+
+class MyCron
+{
+    public function execute()
+    {
+        echo "Bu Cron Job da ne yapılmasını istiyorsanız , bu fonksiyon içine kodları yazınız";
+    }
+}
